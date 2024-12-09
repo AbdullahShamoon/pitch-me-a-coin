77fb2e1c31a1846c0e5243d61108cfdffe3e6c1b
Abdullah <abdullahshamoon24@gmail.com>
2024-12-09 12:03:05 +0530

    change

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,18 +1,23 @@
 'use client'
-import React , { useEffect , useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '@/components/Navbar'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/navigation'
+import { fetchuser , updateUser } from '@/actions/useractions'
 
 
 const page = () => {
-    const { data: session } = useSession()
+    const { data: session , update} = useSession()
     const router = useRouter()
     const [formData, setFormData] = useState({})
 
     const handleChange = (e) => {
         const { name, value } = e.target
         setFormData({ ...formData, [name]: value })
+    }
+
+    const handleSubmit = async (e) => {
+
     }
 
     useEffect(() => {
@@ -45,69 +50,27 @@ const page = () => {
 
                                 {/* Form  */}
 
-                                <form>
+                                <form onSubmit={handleSubmit}>
                                     {/* <!-- Cover Image --> */}
-                                    <div
-                                        className="w-full rounded-sm bg-[url('/cover.jpg')] bg-cover bg-center bg-no-repeat items-center">
+                                    <div className="w-full rounded-sm bg-[url('/cover.jpg')] bg-cover bg-center bg-no-repeat items-center py-4">
                                         {/* <!-- Profile Image --> */}
-                                        <div
-                                            className={`mx-auto flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-[url('/profile.jpg')] bg-cover bg-center bg-no-repeat`}>
-
-                                            <div className="bg-white/90 rounded-full w-6 h-6 text-center ml-28 mt-4">
-
-                                                {/* <input type="file" name="profile" id="upload_profile" hidden required /> */}
-
-                                                <label htmlFor="upload_profile">
-                                                    <svg data-slot="icon" className="w-6 h-5 text-blue-700" fill="none"
-                                                        strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24"
-                                                        xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z">
-                                                        </path>
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z">
-                                                        </path>
-                                                    </svg>
-                                                </label>
-                                            </div>
+                                        <div className={`mx-auto flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-[url('/profile.jpg')] bg-cover bg-center bg-no-repeat`}>
                                         </div>
-                                        <div className="flex justify-end">
-
-                                            {/* <input type="file" name="profile" id="upload_cover" hidden required /> */}
-
-                                            <div
-                                                className="bg-white flex items-center gap-1 rounded-tl-md px-2 text-center font-semibold">
-                                                <label htmlFor="upload_cover" className="inline-flex items-center gap-1 cursor-pointer">Cover
-
-                                                    <svg data-slot="icon" className="w-6 h-5 text-blue-700" fill="none" strokeWidth="1.5"
-                                                        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                                                        aria-hidden="true">
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z">
-                                                        </path>
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z">
-                                                        </path>
-                                                    </svg>
-                                                </label>
-                                            </div>
-
-                                        </div>
+                                        
                                     </div>
-                                    <h2 className="text-center mt-1 font-semibold dark:text-gray-300">Upload Profile and Cover Image
-                                    </h2>
+                                    
                                     <div className="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col gap-2 justify-center w-full">
                                         <div className="w-full  mb-4 mt-6">
                                             <label htmlFor="" className="mb-2 dark:text-gray-300">Full Name</label>
                                             <input type="text"
                                                 className="mt-2 p-4 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                                                placeholder={session?.user?.name} />
+                                                placeholder={session?.user?.name || "Enter your Full Name"} />
                                         </div>
                                         <div className="w-full  mb-4 lg:mt-6">
                                             <label htmlFor="" className=" dark:text-gray-300">Email</label>
                                             <input type="text"
                                                 className="mt-2 p-4 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                                                placeholder={session?.user?.email} />
+                                                placeholder={session?.user?.email || "Enter your Email"} />
                                         </div>
                                     </div>
 
@@ -116,7 +79,7 @@ const page = () => {
                                             <label htmlFor="" className="mb-2 dark:text-gray-300">Username</label>
                                             <input type="text" onChange={handleChange} name='username'
                                                 className="mt-2 p-4 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                                                placeholder="Username" />
+                                                placeholder="Enter a Username" />
                                         </div>
                                         <div className="w-full">
                                             <h3 className="dark:text-gray-300 mb-2">Date Of Birth</h3>
@@ -129,13 +92,27 @@ const page = () => {
                                             <label htmlFor="" className="mb-2 dark:text-gray-300">Profile Pic</label>
                                             <input type="text" onChange={handleChange} name='profile_pic'
                                                 className="mt-2 p-4 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                                                placeholder="Profile pic URL" />
+                                                placeholder="Enter Profile pic URL" />
                                         </div>
                                         <div className="w-full  mb-4 lg:mt-6">
                                             <label htmlFor="" className=" dark:text-gray-300">Cover Pic</label>
                                             <input type="text" onChange={handleChange} name='cover_pic'
                                                 className="mt-2 p-4 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                                                placeholder="Cover pic URL" />
+                                                placeholder="Enter Cover pic URL" />
+                                        </div>
+                                    </div>
+                                    <div className="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col gap-2 justify-center w-full">
+                                        <div className="w-full  mb-3 mt-3">
+                                            <label htmlFor="" className="mb-2 dark:text-gray-300">Mobile Number</label>
+                                            <input type="text" onChange={handleChange} name='mobile_number'
+                                                className="mt-2 p-4 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
+                                                placeholder="Enter your Mobile no." />
+                                        </div>
+                                        <div className="w-full  mb-3 lg:mt-3">
+                                            <label htmlFor="" className=" dark:text-gray-300">Address</label>
+                                            <input type="text" onChange={handleChange} name='address'
+                                                className="mt-2 p-4 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
+                                                placeholder="Enter your Address" />
                                         </div>
                                     </div>
                                     <div className="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col gap-2 justify-center w-full">
